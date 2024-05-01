ec4796cd4c5c603d21d665a14bb764f6fb203ccd
Nick Hageman <nickhageman8@gmail.com>
2024-04-30 23:58:26 -0500

    removed incorrect INO file

--- espController/retroController.ino
+++ espController/retroController.ino
@@ -1,14 +1,53 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClientSecure.h>
+#include <WebSocketsClient.h>
 
-// This file is used for testing connecting to WiFi
+#define BUTTON1_PIN 5  // up button GPIO5 = D1
+#define BUTTON2_PIN 4 // left button, GPIO4 = D2
+#define BUTTON3_PIN 0  // down button, GPIO0 = D3
+#define BUTTON4_PIN 2 // right button, GPIO2 = D4
+#define BUTTON5_PIN 14 // right button, GPIO14 = D5
 
-const char* ssid = "iPhone (2)"; // Replace with your WiFi network name
-const char* password = "nickisthebest"; // Replace with your WiFi password
+// variables for reading the button status
+int button1_state = 0;
+int button2_state = 0;
+int button3_state = 0;
+int button4_state = 0;
+int button5_state = 0;
+
+//const char* ssid = "iPhone (2)"; // Replace with your WiFi network name
+//const char* password = "nickisthebest"; // Replace with your WiFi password
+const char* ssid = "nate"; // Replace with your WiFi network name
+const char* password = "12345678"; // Replace with your WiFi password
+
+WebSocketsClient webSocket; // WebSocket client instance
+
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+  switch(type) {
+    case WStype_DISCONNECTED:
+      Serial.printf("[WebSocket] Disconnected!\n");
+      break;
+    case WStype_CONNECTED:
+      Serial.printf("[WebSocket] Connected to: %s\n", payload);
+      break;
+    case WStype_TEXT:
+      Serial.printf("[WebSocket] Received text: %s\n", payload);
+      break;
+  }
+}
 
 void setup() {
-  Serial.begin(9600); // Start the serial communication
+  Serial.begin(9600); // Start serial communication
+
+  // Configure the ESP8266 pin as a pull-up input: HIGH when the button is open, LOW when pressed.
+  pinMode(BUTTON1_PIN, INPUT_PULLUP);
+  pinMode(BUTTON2_PIN, INPUT_PULLUP);
+  pinMode(BUTTON3_PIN, INPUT_PULLUP);
+  pinMode(BUTTON4_PIN, INPUT_PULLUP);
+  pinMode(BUTTON5_PIN, INPUT_PULLUP);
+
+  // Setup WiFi
   WiFi.begin(ssid, password); // Connect to the WiFi network
 
   Serial.print("Connecting");
@@ -18,33 +57,56 @@ void setup() {
   }
 
   Serial.println("\nConnected to WiFi");
-  makeHTTPSRequest(); // Make the HTTPS request
+
+  // Setup WebSocket
+  webSocket.begin("172.23.5.70", 8080, "/"); // WebSocket server address and port
+  webSocket.onEvent(webSocketEvent);
+  webSocket.setReconnectInterval(5000); // Try to reconnect every 5 seconds
+
 }
 
 void loop() {
-  // Keep the function here to make repeated requests or use deep sleep for battery-driven applications
-}
+  webSocket.loop(); // Allow WebSocket client to process events
 
-void makeHTTPSRequest() {
-  WiFiClientSecure client; // Create a WiFiClientSecure object
-  HTTPClient https; // Declare an object of class HTTPClient
+  // read the state of the button value:
+  button1_state = digitalRead(BUTTON1_PIN);
+  button2_state = digitalRead(BUTTON2_PIN);
+  button3_state = digitalRead(BUTTON3_PIN);
+  button4_state = digitalRead(BUTTON4_PIN);
+  button5_state = digitalRead(BUTTON5_PIN);
 
-  client.setInsecure(); // Use this for testing purposes (ignores SSL certificate errors)
-  
-  if (https.begin(client, "https://www.google.com")) { // Specify request destination
-    int httpCode = https.GET(); // Send the request
-
-    if (httpCode > 0) { // Check the returning code
-      String payload = https.getString(); // Get the request response payload
-      Serial.println("Received payload:");
-      Serial.println(payload);
-    } else {
-      Serial.print("Error on HTTP request: ");
-      Serial.println(httpCode);
-    }
-
-    https.end(); // Close connection
-  } else {
-    Serial.println("Unable to connect");
+  // control LEDs according to the state of button
+  if (button1_state == LOW) { // button is pressed
+    Serial.println("Button 1 PRESSED - UP");
+    webSocket.sendTXT("...."); // up = 4
+    delay(200); // Debounce delay
+    return; // only want one command at a time so break out of the loop iteration
   }
+  if (button2_state == LOW) {
+    Serial.println("Button 2 PRESSED - LEFT");
+    delay(200); // Debounce delay
+    webSocket.sendTXT("..."); // left = 3
+    return; // only want one command at a time so break out of the loop iteration
+  }
+  if (button3_state == LOW) {
+    Serial.println("Button 3 PRESSED - DOWN");
+    webSocket.sendTXT(".."); // down = 2
+    delay(200); // Debounce delay
+    return; // only want one command at a time so break out of the loop iteration
+  }
+  if (button4_state == LOW) {
+    Serial.println("Button 4 PRESSED - RIGHT");
+    webSocket.sendTXT("."); // right = 1
+    delay(200); // Debounce delay
+    return; // only want one command at a time so break out of the loop iteration
+  }
+  if (button5_state == LOW) {
+    Serial.println("Button 5 PRESSED - ENTER");
+    webSocket.sendTXT("....."); // right = 1
+    delay(200); // Debounce delay
+    return; // only want one command at a time so break out of the loop iteration
+  }
+   
+  delay(200); // Debounce delay
+
 }
